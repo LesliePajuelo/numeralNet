2d1467c81849d9fed06e9559f4a1a912e5985c79
Your Name <l.l.pajuelo@gmail.com>
2015-11-24 15:35:43 -0800

    exporting brain

--- js/brainTrain.js
+++ js/brainTrain.js
@@ -9,74 +9,60 @@ fs.readFile(__dirname + '/../assets/mnist_test.csv', function(err, data){
     throw err;
   } else {
     var csvData = data.toString().split("\n");
-    for(var i = 0; i < csvData.length; i++) {
+    for(var i = 0; i < 2; i++) {
         
       testDataArray.push(csvData[i].split(','));
       //console.log(csvData[i].length);
       
     }
-  testData(testDataArray[0]);
-  //console.log(testDataArray[0]);
+  testData(testDataArray);
   }
 });
 
+
+
+var output = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,84,185,159,151,60,36,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,222,254,254,254,254,241,198,198,198,198,198,198,198,198,170,52,0,0,0,0,0,0,0,0,0,0,0,0,67,114,72,114,163,227,254,225,254,254,254,250,229,254,254,140,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,17,66,14,67,67,67,59,21,236,254,106,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,83,253,209,18,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,22,233,255,83,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,129,254,238,44,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,59,249,254,62,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,133,254,187,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,205,248,58,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,126,254,182,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,75,251,240,57,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,19,221,254,166,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,203,254,219,35,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,38,254,254,77,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,31,224,254,115,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,133,254,254,52,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,61,242,254,254,52,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,121,254,254,219,40,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,121,254,207,18,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+var net = new brain.NeuralNetwork();
 var testData = function(data) {
-  var label = parseInt(data.shift());
-  console.log(typeof label)
-  var decimalizedData = data.map(function(num){ return parseFloat((parseInt(num)/255).toFixed(2)) } );
-  // console.log(label, decimalizedData)
+
+    var labels;
+    var pictures;
+    var testTrainingData = [];
+    data.forEach(function(arr) {
+      //setting lables as index flags in output array
+      var output = Array.apply(null, Array(10)).map(Number.prototype.valueOf, 0); 
+      labels = parseInt(arr.shift())
+      output[labels] = 1;
+
+
+      //converts 0-255 values to truncated decimals
+      pictures = arr.map(function(num){ return parseInt(num) } );
+      testTrainingData.push( { input: pictures, output: output } );
+    });
+    net.train(testTrainingData);
+    // console.log(net.train(testTrainingData));
+    return json = net.toJSON();
+  
 }
 
-
-// var trainingData = fs.readFile(__dirname + '/../assets/mnist_train.csv', function(err, data){
-//     if(err) {
-//         throw err;
-//     } else{
-//         return data
-//     }
-// });
-
-
-// It would be nice with a checker instead of a hard coded 60000 limit here
-// for (var image = 0; image <= 0; image++) { 
-//     var pixels = [];
-
-//     for (var x = 0; x <= 27; x++) {
-//         for (var y = 0; y <= 27; y++) {
-//             // console.log(testData[(image * 28 * 28) + (x + (y * 28)) + 15]);
-//             pixels.push(testData[(image * 28 * 28) + (x + (y * 28)) + 15]);
-//         }
-//     }
-
-//     var imageData  = {};
-//     imageData[JSON.stringify(trainingData[image + 9])] = pixels;
-//     for (var p = 0; p < 100; p++) {
-//     console.log(parseInt(trainingData[image + p]));
-//     }
-//     // console.log(imageData);
-//     pixelValues.push(imageData);
-// }
-// // console.log(pixelValues[0]);
-
-
-
-
-
+var trainedNetwork = function() {
+  console.log(net.run(output));
+};
 
 
 // var net = new brain.NeuralNetwork();
 
 // net.train([
-//            {input: [0, 0], output: [0]},
-//            {input: [0, 1], output: [1]},
-//            {input: [1, 0], output: [1]},
-//            {input: [1, 1], output: [0]},
-//            {input: [1, 0], output: [1]}
+//            {input: [0, 0], output: ['0']},
+//            {input: [0, 1], output: ['10']},
+//            {input: [1, 0], output: ['10']},
+//            {input: [1, 1], output: ['0']},
+//            {input: [1, 0], output: ['10']}
 // ])
 
-// var output = net.run([1,1])
+// var output = net.run([1,0])
 
-// //console.log(output)
+// console.log(output)
 // //---------------------------------------------
 // // Teach it math:
 // //---------------------------------------------

a713a5f2909518a09c28da381f1fce1cba11b34e
Leslie Pajuelo <l.l.pajuelo@gmail.com>
2015-11-27 20:50:04 -0800

    Fix canvas clearing issue

--- app/controllers/imageTesting.js
+++ app/controllers/imageTesting.js
@@ -2,7 +2,10 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d')
 
 var erase = function(){
-  context.clearRect(0,0, context.canvas.width, context.canvas.height)
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  clickX = [];
+  clickY = [];
+  clickDrag = [];
 };
 
 //Get mouse information

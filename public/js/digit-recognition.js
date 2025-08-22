39c9df34a4f261c0321de2a73845887c984c487c
Leslie <leslie.pajuelo@numerator.com>
2025-08-22 16:11:24 -0400

    Try smaller neural network data file to fix deployment issue

--- public/js/digit-recognition.js
+++ public/js/digit-recognition.js
@@ -186,7 +186,7 @@ var recognize = function() {
 
 function loadTrainedNetwork() {
 // TODO RUPA/LP swap out this file. 
-  fetch('fourthBrainData.json')
+  fetch('testBrainData.json')
     .then(response => {
       if (!response.ok) {
         throw new Error('Failed to load neural network data');
